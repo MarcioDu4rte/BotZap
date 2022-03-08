7005ed45712f6845460a40335fa95623044077ee
MarcioDu4rte <mdssolucoes@gmail.com>
2022-03-08 14:13:32 -0300

    Segunda Versao

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,4 +1,4 @@
-#Importar as bibliotecas
+# Importar as bibliotecas
 from selenium import webdriver
 import time
 import webdriver_manager
@@ -11,21 +11,25 @@ driver.get('https://web.whatsapp.com/')
 time.sleep(30)
 
 # definir contatos e grupos e mensagem a ser enviada
-contatos = ['Grupo só meu','Édina Comercial']
+contatos = ['Grupo só meu', 'Édina Comercial']
 mensagem = 'Teste envio mensagem automatizada!'
 
 # Buscar contatos ou grupos
 
+
 def buscar_contato(contato):
-    campo_pesquisa = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
+    campo_pesquisa = driver.find_element_by_xpath(
+        '//*[@id="side"]/div[1]/div/label/div/div[2]')
     time.sleep(2)
     campo_pesquisa.click()
     campo_pesquisa.send_keys(contato)
     campo_pesquisa.send_keys(Keys.ENTER)
     time.sleep(1)
 
+
 def enviar_mensagem(mensagem):
-    campo_mensagem = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+    campo_mensagem = driver.find_element_by_xpath(
+        '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
     time.sleep(2)
     campo_mensagem.click()
     campo_mensagem.send_keys(mensagem)
@@ -34,6 +38,7 @@ def enviar_mensagem(mensagem):
 
 # Enviar mensagens para o contato ou grupo
 
+
 for contato in contatos:
     buscar_contato(contato)
     enviar_mensagem(mensagem)
@@ -41,5 +46,3 @@ for contato in contatos:
 # xpath busca: //*[@id="side"]/div[1]/div/label/div/div[2]
 
 # xpath enviar mensagem: //*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]
-
-
